d0080350db95b62113d65af35b33f3029165bdf3
Akshay <akska8@gmail.com>
2012-01-13 14:50:36 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slargu::Application.config.secret_token = '13deaef665e2a285c60c8bb07657784a7e3d6a75986150780916298e89d5ee8cf6445aaf00c80135cf41e9e9fd4610f7ceec9ae34f523dbd944de34e82db5585'
